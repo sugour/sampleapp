dbca091c4c6546521859a26d930554003ecc9280
sugour <sugour@DIN52005126.corp.capgemini.com>
2021-08-17 10:54:09 +0530

    sales display method added with sum

--- firstdemo/src/main/java/firstdemo/project1.java
+++ firstdemo/src/main/java/firstdemo/project1.java
@@ -1,6 +1,14 @@
 package firstdemo;
 
 public class project1 {
-	public
+	public void display() {
+		int[] arr= {34,54,3254,34,32,34};
+		int sum=0;
+		for(int num:arr) {
+			sum+=num;
+			System.out.println(num);
+		}
+		System.out.println("smmation:"+sum);
+	}
 
 }
